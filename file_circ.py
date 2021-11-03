aaf44f1c96a787465d19fcf4a164935fce73c8e6
BuildTools <unconfigured@null.spigotmc.org>
2021-11-03 20:35:29 +0100

    pep8 sur file_circ

--- file_circ.py
+++ file_circ.py
@@ -1,14 +1,22 @@
 class File_circ:
+    """
+    classe définissant une file circulaire
+    ---
+    ATTRIBUTS :
+    file : liste représentant la file
+    tmax : le nombre d'élément de la file
+    index_début : indique quel est le premier élément de la file
+    index_fin : indique quel est le dernier élement de la file
+    ---
+    METHODES :
+    est_vide : renvoie True si la file est vide, False sinon
+    enfiler: enfile un élément dans la file
+    defiler : defile l'élément à sortir selon la règle du
+        premier entré, premier sorti
+    nombre_elements : renvoie le nombre d'élément présent dans la file
+    """
 
     def __init__(self, n):
-        """
-        ATTRIBUTS :
-            classe définissant une file circulaire
-        file : liste représentant la file
-        tmax : le nombre d'élément de la file
-        index_début : indique quel est le premier élément de la file
-        index_fin : indique quel est le dernier élement de la file
-        """
         self.file = [None for x in range(n)]
         self.tmax = n
         self.index_debut = 0
@@ -16,12 +24,19 @@ class File_circ:
         self.nbr_elements = 0
 
     def est_vide(self):
+        """
+        renvoie True si la file est vide, False sinon
+        """
         if self.nbr_elements == 0:
             return True
         else:
             return False
 
     def enfiler(self, element):
+        """
+        enfile l'objet "element" dans la file selon la règle du
+            premier entré, premier sorti
+        """
         if self.nbr_elements < 6:
             self.file[self.index_fin] = element
             self.nbr_elements += 1
@@ -31,6 +46,10 @@ class File_circ:
                 self.index_fin = 0
 
     def defiler(self):
+        """
+        defile l'objet prêt à sortir de la file selon
+            la règle du premier entré, premier sorti
+        """
         if self.est_vide() is False:
             element = self.file[self.index_debut]
             if self.index_debut == 5:
@@ -41,6 +60,9 @@ class File_circ:
             return element
 
     def nombre_elements(self):
+        """
+        renvoie le nombre d'élément présent dans la file
+        """
         nbr = self.index_fin-self.index_debut
         if nbr == - 1:
             nbr = 6
