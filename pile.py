a6e529895e7a4348d12fbfbaad3106a1ea54a26b
BuildTools <unconfigured@null.spigotmc.org>
2021-11-04 16:52:53 +0100

    pep8 sur pile.py

--- pile.py
+++ pile.py
@@ -1,20 +1,21 @@
 class pile:
     """ classe définissant le tad PILE
+        ---
         ATTRIBUTS :
                 p : LISTE : Contenu de la pile
-                nbr_elements : ENTIER : nombre d'éléments dans la pile (accessoire)
-                
+                nbr_elements: nombre d'éléments dans la pile (accessoire)
+        ---
         METHODES :
-            est_vide : -> BOOLEEN : retourne VRAI si la pile est vide, FAUX sinon.
+            est_vide : -> BOOLEEN : return VRAI si la pile est vide, FAUX sinon
             empiler : ELEMENT -> VOID : ajoute un élément dans la pile
             depiler : -> ELEMENT : retire l'élément de la tête de la pile.
     """
-    
+
     def __init__(self):
-        self.p=[]
-        self.nbr_elements=0
-        
-    def empiler(self,element):
+        self.p = []
+        self.nbr_elements = 0
+
+    def empiler(self, element):
         """
         méthode qui empile l'élément element dans la file
         -----
@@ -23,38 +24,43 @@ class pile:
         pas de renvoie
         """
         self.p.append(element)
-        self.nbr_elements+=1
-        
+        self.nbr_elements += 1
+
     def est_vide(self):
+        """
+        vérifie si la file est vide
+        ---
+        renvoie True si elle l'est, False sinon
+        """
         return self.nbr_elements == 0
-        
-    
+
     def depiler(self):
-        element=None
+        element = None
         if not self.est_vide():
             element = self.p[-1]
             del(self.p[-1])
-            self.nbr_elements-=1
+            self.nbr_elements -= 1
         return element
-    
+
     def dernier(self):
         element = None
         if not self.est_vide():
             element = self.p[-1]
         return element
-    
+
     def visualiser(self):
         liste = []
         for x in self.p:
-            print("PILE : ",x)
+            print("PILE : ", x)
             liste.append(x)
         print(liste)
         return liste
 
-if __name__=="__main__":  
-  
-    p=pile()
-    print(isinstance(p,int))
+
+if __name__ is "__main__":
+
+    p = pile()
+    print(isinstance(p, int))
     print(p.est_vide())
     p.empiler("UP")
     p.empiler("DOWN")
