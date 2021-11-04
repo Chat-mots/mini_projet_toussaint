f1799d0017f59195caafd96d45828aabc2dc7a21
Nbrehin <80753480+Nbrehin@users.noreply.github.com>
2021-11-04 17:47:57 +0100

    MAJ
    Ajouts de commentaires + Ajouts d'asserts

--- pile.py
+++ pile.py
@@ -17,9 +17,9 @@ class pile:
 
     def empiler(self, element):
         """
-        méthode qui empile l'élément element dans la file
+        méthode qui empile l'élément element dans la pile
         -----
-        element : n'importe quoi qui peut être mis dans une file
+        element : n'importe quoi qui peut être mis dans une pile
         -----
         pas de renvoie
         """
@@ -28,13 +28,20 @@ class pile:
 
     def est_vide(self):
         """
-        vérifie si la file est vide
+        vérifie si la pile est vide
         ---
         renvoie True si elle l'est, False sinon
         """
         return self.nbr_elements == 0
 
     def depiler(self):
+        """
+        méthode qui dépile l'élément element de la pile
+        -----
+        element : n'importe quoi, présent dans la pile
+        -----
+        aucun renvoie
+        """
         element = None
         if not self.est_vide():
             element = self.p[-1]
@@ -43,12 +50,26 @@ class pile:
         return element
 
     def dernier(self):
+        """
+        méthode qui affiche l'élément element au sommet de la pile
+        -----
+        element : peut-être n'importe quoi, empilé au sommet de la pile
+        -----
+        renvoie l'élément en tête de pile
+        """
         element = None
         if not self.est_vide():
             element = self.p[-1]
         return element
 
     def visualiser(self):
+        """
+        méthode affichant, élément par élément, le contenu de la pile
+        -----
+        liste : prend un a un les éléments de la pile
+        -----
+        renvoie les éléments de la pile un par un
+        """
         liste = []
         for x in self.p:
             print("PILE : ", x)
@@ -57,17 +78,22 @@ class pile:
         return liste
 
 
-if __name__ is "__main__":
+if __name__ == "__main__":
 
     p = pile()
     print(isinstance(p, int))
+    assert(p.est_vide()) == True
+    assert(p.dernier()) == None
     print(p.est_vide())
     p.empiler("UP")
+    assert(p.est_vide()) == False
     p.empiler("DOWN")
+    assert(p.dernier()) == "DOWN"
     print(p.depiler())
     p.empiler("TRIGHT")
     print(p.est_vide())
     print(p.depiler())
     print(p.est_vide())
     p.empiler("INIT")
+    assert(p.est_vide()) == False
     p.visualiser()
