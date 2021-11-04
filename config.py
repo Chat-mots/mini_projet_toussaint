da4142752ae2ad867997370b21c76b383e14276f
BuildTools <unconfigured@null.spigotmc.org>
2021-11-04 16:56:38 +0100

    pep8 sur config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 
-commandes_base = ["UP","DOWN","TRIGHT","TLEFT","JOPEN","JCLOSE","VCENTER","HCENTER"]
-commandes_prog = ["FIN","REM","VIEW"]
+commandes_base = ["UP", "DOWN", "TRIGHT", "TLEFT", "JOPEN", "JCLOSE", "VCENTER", "HCENTER"]
+commandes_prog = ["FIN", "REM", "VIEW"]
 
-fich_sr="sr.txt"
-commandes_sr=[]
+fich_sr = "sr.txt"
+commandes_sr = []
